7077a56c56c94e721842d405bfd219fe9721d440
JJJJ217 <169433769+JJJJ217@users.noreply.github.com>
2026-04-29 10:21:57 +1000

    add cinemas

---
+++ UTS Cinemas/UTS Cinemas/Model/Cinemas.swift
@@ -0,0 +1,15 @@
+//
+//  Cinemas.swift
+//  UTS Cinemas
+//
+//  Created by Jiaming Huang on 29/4/2026.
+//
+
+import Foundation
+
+struct Cinemas: Identifiable {
+    let id = UUID()
+    var name: String
+    var genre: String
+    var address: String
+}
