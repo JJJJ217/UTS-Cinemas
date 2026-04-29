2499a0ee2e6c6e74c88f4b64c1d66c8d973cc32d
JJJJ217 <169433769+JJJJ217@users.noreply.github.com>
2026-04-29 10:05:23 +1000

    test

---
+++ UTS Cinemas/UTS Cinemas/UTS_CinemasApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTS_CinemasApp.swift
+//  UTS Cinemas
+//
+//  Created by Jiaming Huang on 29/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct UTS_CinemasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UTS Cinemas/UTS CinemasTests/UTS_CinemasTests.swift
@@ -0,0 +1,17 @@
+//
+//  UTS_CinemasTests.swift
+//  UTS CinemasTests
+//
+//  Created by Jiaming Huang on 29/4/2026.
+//
+
+import Testing
+@testable import UTS_Cinemas
+
+struct UTS_CinemasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
